278f64b0e25a3dbdb6cb64f10ea6890c87808c37
pedroaraya33 <paraya@ticblue.com>
2021-09-30 16:41:26 -0300

    modificaciones iphone x y más

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // Todo lo que este en inmutable nosotros no lo vamos a modificar
